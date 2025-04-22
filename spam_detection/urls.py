2bece7592a137a27d5d21a2f72ba0d1c2dc715d6
Pooja Chauhan <inbox4poojaa@gmail.com>
2025-04-22 10:43:32 -0500

    Initial commit

--- spam_detection/urls.py
+++ spam_detection/urls.py
@@ -4,6 +4,6 @@ from detection.views import home, check_spam
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # Home page
-    path('check-spam/', check_spam, name='check_spam'),  # Spam detection page
+    path('', home, name='home'), 
+    path('check-spam/', check_spam, name='check_spam'), 
 ]
